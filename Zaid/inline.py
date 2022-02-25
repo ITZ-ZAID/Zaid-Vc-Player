27fb5f6252462c8423a91655d3607a1e6e7b4e4e
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-02-25 17:49:59 +0530

    Add files via upload

---
+++ Zaid/inline.py
@@ -0,0 +1,62 @@
+""" inline section button """
+
+from pyrogram.types import (
+  CallbackQuery,
+  InlineKeyboardButton,
+  InlineKeyboardMarkup,
+  Message,
+)
+
+
+def stream_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data=f'cbmenu | {user_id}'),
+      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data=f'cls'),
+    ],
+    [
+      InlineKeyboardButton(text="✨ ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP_SUPPORT}"),
+      InlineKeyboardButton(text="📣 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"),
+    ],
+  ]
+  return buttons
+
+
+def menu_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="⏹", callback_data=f'cbstop | {user_id}'),
+      InlineKeyboardButton(text="⏸", callback_data=f'cbpause | {user_id}'),
+      InlineKeyboardButton(text="▶️", callback_data=f'cbresume | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🔇", callback_data=f'cbmute | {user_id}'),
+      InlineKeyboardButton(text="🔊", callback_data=f'cbunmute | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🗑 Close", callback_data='cls'),
+    ]
+  ]
+  return buttons
+
+
+close_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "🗑 Close", callback_data="cls"
+      )
+    ]
+  ]
+)
+
+
+back_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "🔙 Go Back", callback_data="cbmenu"
+      )
+    ]
+  ]
+)
