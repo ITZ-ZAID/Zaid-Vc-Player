d1e02a878994630d5bdd4771883bd091bd5d1969
Broken-Mr-z <100361408+Broken-Mr-z@users.noreply.github.com>
2022-03-14 21:39:19 +0530

    Update video.py

--- Zaid/Player/video.py
+++ Zaid/Player/video.py
@@ -54,7 +54,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["vplay", f"vplay@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["vplayy", f"vplay@{BOT_USERNAME}"]) & other_filters)
 async def vplay(c: Client, m: Message):
     await m.delete()
     replied = m.reply_to_message
@@ -318,7 +318,7 @@ async def vplay(c: Client, m: Message):
                             await m.reply_text(f"🚫 error: `{ep}`")
 
 
-@Client.on_message(command(["vstream", f"livestream"]) & other_filters)
+@Client.on_message(command(["vstreamm", f"livestream"]) & other_filters)
 async def vstream(c: Client, m: Message):
     await m.delete()
     chat_id = m.chat.id
