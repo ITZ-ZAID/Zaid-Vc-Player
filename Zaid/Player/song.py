2f783fa38c4bb38a28c23ab20ec17b838e0c61e8
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-06 13:52:21 +0530

    Update song.py

--- Zaid/Player/song.py
+++ Zaid/Player/song.py
@@ -286,10 +286,15 @@ async def lyrics(_, message):
         await rep.edit("❌ **lyrics not found.**\n\n» **please give a valid song name.**")
 
 
-@Client.on_message(command(["restart", f"update"]))
-async def restart(_, message):
-    user_id = message.from_user.id
-    if user_id != SUDO_USERS:
+@Client.on_message(command(["restart", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
+async def restart_bot(_, message: Message):
+    if message.from_user.id not in SUDO_USERS:
         return
-    await message.reply_text("🛠 <i>Restarting Player...</i>")
+    try:
+        msg = await message.reply_text("❖ Restarting bot...")
+        LOGS.info("[INFO]: BOT SERVER RESTARTED !!")
+    except BaseException as err:
+        LOGS.info(f"[ERROR]: {err}")
+        return
+    await msg.edit_text("✅ Bot has restarted !\n\n» back active again in 5-10 seconds.")
     os.system(f"kill -9 {os.getpid()} && python3 main.py")
