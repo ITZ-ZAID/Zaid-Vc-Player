cf23e7346460a880eeb9e7da6c6bef965e5e86bf
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-06 10:38:01 +0530

    Update play.py

--- Zaid/Player/play.py
+++ Zaid/Player/play.py
@@ -51,9 +51,10 @@ def ytsearch(query: str):
         print(e)
         return 0
 
-
-async def ytdl(format: str, link: str):
-    stdout, stderr = await bash(f'youtube-dl -g -f "{format}" {link}')
+async def ytdl(link: str):
+    stdout, stderr = await bash(
+        f'yt-dlp --geo-bypass -g -f "[height<=?720][width<=?1280]" {link}'
+    )
     if stdout:
         return 1, stdout
     return 0, stderr
