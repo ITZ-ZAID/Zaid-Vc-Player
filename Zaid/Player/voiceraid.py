18e4765741821e472b09b52d27475eae2c63ee9b
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-08 20:29:29 +0530

    Update voiceraid.py

--- Zaid/Player/voiceraid.py
+++ Zaid/Player/voiceraid.py
@@ -16,7 +16,7 @@ from pytgcalls.types.input_stream.quality import (HighQualityAudio, HighQualityV
 
 from Zaid.queues import QUEUE, add_to_queue, get_queue, clear_queue
 
-from Zaid.main import call_py, bot as vcbot
+from Zaid.main import call_py as call_py1, bot as vcbot, Venom1
 from config import SUDO_USERS
 
 logging.basicConfig(level=logging.INFO)
@@ -36,33 +36,30 @@ aud_list = [
     "./Zaid/Audio/AUD10.mp3",
 ]
 
-Venom1 = call_py
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["vcraid"], prefixes=HNDLR))
 async def vcraid(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
-    inp = e.text[8:]
+    if gid == uid:
+        inp = e.text[8:]
+        chat_ = await Venom1.get_chat(inp)
+        chat_id = chat_.id
+    else:
+         chat_id = gid
     aud = choice(aud_list)
-    chat = e.text.split(None, 2)[1]
-    try:
-        chat_id = (await call_py.get_chat(chat)).id
-        await call_py.join_chat(chat_id)
-        Zaid = await e.reply_text("**Starting VC raid**")
+    if inp:
+        TheVenomXD = await e.reply_text("**Starting VC raid**")
         link = f"https://itshellboy.tk/{aud[1:]}"
         dl = aud
         songname = aud[18:]
         if chat_id in QUEUE:
             pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-            await Zaid.delete()
+            await TheVenomXD.delete()
             await e.reply_text(f"**> Raiding in:** {chat_.title} \n\n**> Audio:** {songname} \n**> Position:** #{pos}")
-            await call_py.join_group_call(chat_id, AudioPiped(dl), stream_type=StreamType().pulse_stream)
+        else:
+            if call_py1:
+                await call_py1.join_group_call(chat_id, AudioPiped(dl), stream_type=StreamType().pulse_stream)
             add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-            await Zaid.delete()
+            await TheVenomXD.delete()
             await e.reply_text(f"**> Raiding in:** {chat_.title} \n\n**> Audio:** {songname} \n**> Position:** Ongoing Raid")
-
-
-
-
-
-
