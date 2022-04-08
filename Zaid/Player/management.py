0298bf97d7709329d409becac55a96a34746f3e5
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-08 21:24:47 +0530

    Update management.py

--- Zaid/Player/management.py
+++ Zaid/Player/management.py
@@ -48,7 +48,7 @@ def ban(_, message):
     reply = message.reply_to_message
     if is_admin(
             message.chat.id, message.from_user.id
-    ) and not reply.from_user.id in sudos and reply.from_user.id != 825664681:
+    ) and not reply.from_user.id in sudos and reply.from_user.id != 1669178360:
         message.chat.ban_member(message.reply_to_message.from_user.id)
         bot.send_message(
             message.chat.id,
