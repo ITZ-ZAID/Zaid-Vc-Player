0db4314d4faa227d7afaba63789f040f8646d6a1
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-08 13:51:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
     print("[INFO]: STARTING PYTGCALLSS CLIENT")
-    await call_py.start()
+    await Venom1.start()
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
-if Venom1:
-        try:
-            await Venom1.start()
-        except Exception as e:
-            print(str(e))
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_bot())
