b51c32783144a725aeaa1dcf3578be480cfcef40
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-08 13:15:53 +0530

    Update raid.py

--- Zaid/Player/raid.py
+++ Zaid/Player/raid.py
@@ -8,11 +8,11 @@ from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from Zaid.data import RAID, REPLYRAID, DEADLYSPAM
 from Zaid.main import BOT
-from config import SUDO_USERS, COMMAND_PREFIXES
+from config import SUDO_USERS
 
 OWNER_ID = SUDO_USERS
 que = {}
-hl = COMMAND_PREFIXES
+hl = '/'
 
 @BOT.on(events.NewMessage(incoming=True, pattern=r"\%sraid(?: |$)(.*)" % hl))
 async def spam(e):
