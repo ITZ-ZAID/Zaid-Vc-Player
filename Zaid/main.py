15449e24c9d3620a5ddbd873ce44fb03307fef01
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-08 14:03:41 +0530

    Update main.py

--- Zaid/main.py
+++ Zaid/main.py
@@ -25,12 +25,6 @@ user = Client(
 )
 
 
-if SESSION_NAME:
-    Venom1 = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'Zaid.Player'})
-    call_py1 = PyTgCalls(Venom1)
-else:
-    Venom1 = None
-    call_py1 = None
 
 call_py = PyTgCalls(user, overload_quiet_mode=True)
 
