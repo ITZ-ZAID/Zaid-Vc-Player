02cf7e6e2e550277408dc341b52b336a29cbc7e5
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-15 22:22:59 +0530

    Update userbotjoin.py

--- Zaid/Player/userbotjoin.py
+++ Zaid/Player/userbotjoin.py
@@ -2,7 +2,7 @@ import asyncio
 from config import BOT_USERNAME, SUDO_USERS
 from Zaid.decorators import authorized_users_only, sudo_users_only, errors
 from Zaid.filters import command, other_filters
-from Zaid.main import user as USER
+from Zaid.main import Test as USER
 from pyrogram import filters
 from Zaid.main import bot as Client
 from pyrogram.errors import UserAlreadyParticipant
