1786b19e3e0e63194f2bf6f953742a62472d253f
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-20 22:29:25 +0530

    Okie

--- Zaid/Player/start.py
+++ Zaid/Player/start.py
@@ -1,108 +1,228 @@
 import asyncio
-from time import time
-from datetime import datetime
-from config import BOT_USERNAME
-from config import GROUP_SUPPORT, UPDATES_CHANNEL, START_PIC
-from Zaid.filters import command
-from Zaid.command import commandpro
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from Zaid.main import bot as Client
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import MessageNotModified
+from main import Test, bot as Client
+from config import START_PIC
 
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ('week', 60 * 60 * 24 * 7),
-    ('day', 60 * 60 * 24),
-    ('hour', 60 * 60),
-    ('min', 60),
-    ('sec', 1)
-)
 
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return 'inf'
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append('{} {}{}'
-                         .format(amount, unit, "" if amount == 1 else "s"))
-    return ', '.join(parts)
-    
-   
+ALIVE_PIC = START_PIC
+HOME_TEXT = "👋🏻 **Hi Sir [{}](tg://user?id={})** \n\n🤖 Im **Zaid Vc Player**. \n**I Can Stream Lives, Radios, Raid, Vc Raid, YouTube Videos & Telegram Video Files On Voice Chat Of Telegram Groups**"
+HELP_TEXT = """
+🏷️ **Setup Guide** :
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
-async def start_(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"{START_PIC}",
-        caption=f"""A Telegram Music Bot Based PyroGram.
+\u2022 Start a voice chat in your group.
+\u2022 Add bot and user account in chat with admin rights.
+\u2022 Done Setup Process Read Commands Below 👇.
+"""
 
-💞 **Main Features**
-~ Support Audio + Video Stream
-~ YouTube/Local/Live/m3u8 stream support
-~ Voice Raid / Video Raid
-~ Spam, Bigspam, Raid and LoveRaid
-~ Audio And Video Download from YouTube
 
-Powered By [ᴢᴀɪᴅ ʙᴏᴛꜱ](t.me/superior_bots) ...
-""",
-    reply_markup=InlineKeyboardMarkup(
+
+USER_TEXT = """
+🏷️ **Users Commands** :
+
+\u2022 /play <Query> To Play a Song.
+\u2022 /vplay <Query> To Play Video.
+\u2022 /stream <Live Url> To Play Live Streams 👇\n /song To Download A Audio file from YouTube. \n /video to download Video From YouTube\n /lyric to find Lyrics.
+"""
+
+SPAM_TEXT = """
+🏷️ **Spam Help @adminsOnly** :
+
+\u2022 /spam <Count> Text To Spam Your Message.
+\u2022 /fspam <Count> Text for spamming.
+\u2022 /delayspam <Count> Text for Spamming.
+"""
+
+RAID_TEXT = """
+🏷️ **Raid Commands @SudoOnly** :
+
+\u2022 /vcraid <chatid> - Give a Chat Id Else Username To Voice Raid.
+\u2022 /vraid <chatid + Reply To Video File> - To Raid Video.
+\u2022 /raidpause - To Pause Raid.
+\u2022 /raidresume To Resume Raid.
+\u2022 /raidend <chatid> To End Audio/Video Raid.
+"""
+
+ADMIN = """
+🏷️ **admin Commands** :
+
+\u2022 /userbotjoin To Invite Assistant To Your Chat.
+\u2022 /end To End Streaming.
+\u2022 /pause To Pause Stream.
+\u2022 /resume To Resume Stream.
+\u2022 /volume To Set Volume.
+\u2022 /skip To Skip Tracks.
+"""
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.data=="help":
+        buttons = [
             [
-                [
-                    InlineKeyboardButton(
-                        "➕ ❰ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ❱ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅꜱ", url=f"https://t.me/SUPERIOR_BOTS/160"
-                    ),
-                    InlineKeyboardButton(
-                        "ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url="https://github.com/ITZ-ZAID/Zaid-Vc-Player"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "📢 ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"
-                    ),
-                    InlineKeyboardButton(
-                        "ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 🇮🇳", url=f"https://t.me/{GROUP_SUPPORT}"
-                    )
-                ]
-                
-           ]
-        ),
-    )
-    
-    
-@Client.on_message(commandpro(["/start", "/stats"]) & filters.group & ~filters.edited)
+                InlineKeyboardButton("👮 Aᴅᴍɪɴꜱ", url="https://telegra.ph/𝗕ooo--‌ᴀꜰᴋ-ᴏꜰꜰʟɪɴᴇ-05-17-2"),
+                InlineKeyboardButton("🗨️ Uꜱᴇʀꜱ", callback_data="users"),
+            ],
+            [
+                InlineKeyboardButton("🤬 Rᴀɪᴅ", callback_data="raid"),
+                InlineKeyboardButton("🗨️ Sᴘᴀᴍ", callback_data="spam"),
+            ],
+            [
+                InlineKeyboardButton("🤖 Cʟᴏɴᴇʀ", url="t.me/ZaidClonerBot"),
+            ],
+            [
+                InlineKeyboardButton("🔙 Bᴀᴄᴋ", callback_data="home"),
+                InlineKeyboardButton("🤷 Cʟᴏꜱᴇ", callback_data="close"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                HELP_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="home":
+        get_me = await client.get_me()
+        USERNAME = get_me.username
+        buttons = [
+            [
+                InlineKeyboardButton("🧐 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ", url='https://t.me/{USERNAME}?startgroup=true'),
+            ],
+            [
+                InlineKeyboardButton("💌 Sᴜᴘᴘᴏʀᴛ", url="https://t.me/Superior_Support"),
+                InlineKeyboardButton("🏷️ Cʜᴀɴɴᴇʟ", url="https://t.me/Superior_bots"),
+            ],
+            [
+                InlineKeyboardButton("🤖 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/Itz-Zaid/Zaid-Vc-Player"),
+            ],
+            [
+                InlineKeyboardButton("🤔 Hᴇʟᴘ & Cᴏᴍᴍᴀɴᴅꜱ", callback_data="help"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                HOME_TEXT.format(query.from_user.first_name, query.from_user.id),
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="users":
+        buttons = [
+            [
+                InlineKeyboardButton("🔙 Bᴀᴄᴋ", callback_data="help"),
+                InlineKeyboardButton("🤷 Cʟᴏꜱᴇ", callback_data="close"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                USER_TEXT.format(query.from_user.first_name, query.from_user.id),
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="admins":
+        buttons = [
+            [
+                InlineKeyboardButton("🔙 Bᴀᴄᴋ", callback_data="help"),
+                InlineKeyboardButton("🤷 Cʟᴏꜱᴇ", callback_data="close"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(ADMIN, reply_markup=reply_markup)
+        except MessageNotModified:
+            pass
+
+    elif query.data=="raid":
+        buttons = [
+            [
+                InlineKeyboardButton("🔙 Bᴀᴄᴋ", callback_data="help"),
+                InlineKeyboardButton("🤷 Cʟᴏꜱᴇ", callback_data="close"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                RAID_TEXT.format(query.from_user.first_name, query.from_user.id),
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="spam":
+        buttons = [
+            [
+                InlineKeyboardButton("🔙 Bᴀᴄᴋ", callback_data="help"),
+                InlineKeyboardButton("🤷 Cʟᴏꜱᴇ", callback_data="close"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                SPAM_TEXT.format(query.from_user.first_name, query.from_user.id),
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+
+
+@Client.on_message(filters.command(["start"]) & filters.private)
 async def start(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/dd9ca2b2122dd68ffab0e.png",
-        caption=f"""Thanks For Adding Me To Ur Chat, For Any Query U Can Join Our Support Groups 🔥♥️""",
-        reply_markup=InlineKeyboardMarkup(
+    get_me = await client.get_me()
+    USERNAME = get_me.username
+    buttons = [
             [
-                [
-                    InlineKeyboardButton(
-                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ 💞", url=f"https://t.me/{GROUP_SUPPORT}")
-                ]
+                InlineKeyboardButton("🧐 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ", url=f'https://t.me/{USERNAME}?startgroup=true'),
+            ],
+            [
+                InlineKeyboardButton("💌 Sᴜᴘᴘᴏʀᴛ", url="https://t.me/Superior_Support"),
+                InlineKeyboardButton("🏷️ Oꜰꜰɪᴄɪᴀʟ Cʜᴀɴɴᴇʟ", url="https://t.me/Superior_bots"),
+            ],
+            [
+                InlineKeyboardButton("🤖 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/Itz-Zaid/Zaid-Vc-Player"),
+            ],
+            [
+                InlineKeyboardButton("🤔 Hᴇʟᴘ & Cᴏᴍᴍᴀɴᴅꜱ", callback_data="help"),
             ]
-        ),
-    )
+            ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_photo(photo=f"{ALIVE_PIC}", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
 
-
-@Client.on_message(command(["repo", "source"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["help"]) & filters.private)
 async def help(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/92688f2c44a35ba673c23.png",
-        caption=f"""Here Is The Source Code Fork And Give Stars ✨""",
-        reply_markup=InlineKeyboardMarkup(
+    get_me = await client.get_me()
+    self.username = get_me.username
+    buttons = [
             [
-                [
-                    InlineKeyboardButton(
-                        " ʀᴇᴘᴏ ⚒️", url=f"https://github.com/ITZ-ZAID/Zaid-Vc-Player")
-                ]
+                InlineKeyboardButton("👮 Aᴅᴍɪɴꜱ", url="https://telegra.ph/𝗕ooo--‌ᴀꜰᴋ-ᴏꜰꜰʟɪɴᴇ-05-17-2"),
+                InlineKeyboardButton("🗨️ Uꜱᴇʀꜱ", callback_data="users"),
+            ],
+            [
+                InlineKeyboardButton("🤬 Rᴀɪᴅ", callback_data="raid"),
+                InlineKeyboardButton("🗨️ Sᴘᴀᴍ", callback_data="spam"),
+            ],
+            [
+                InlineKeyboardButton("🤖 Cʟᴏɴᴇʀ", url="t.me/ZaidClonerBot"),
+            ],
+            [
+                InlineKeyboardButton("🔙 Bᴀᴄᴋ", callback_data="home"),
+                InlineKeyboardButton("🤷 Cʟᴏꜱᴇ", callback_data="close"),
             ]
-        ),
-    )
+            ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_photo(photo=f"{ALIVE_PIC}", caption=f"{HELP_TEXT}", reply_markup=reply_markup)
