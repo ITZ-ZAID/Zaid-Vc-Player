983b76dbcea269ad4a49b2d718a32de37c79bfb8
Zaid <itsunknown122@gmail.com>
2022-06-15 09:16:04 +0530

    Add files via upload

--- Zaid/main.py
+++ Zaid/main.py
@@ -4,9 +4,11 @@ import random
 import asyncio
 import telethon.utils
 from telethon import TelegramClient, events
-from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
+from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME, SESSION2, SESSION3, SESSION4, SESSION5
 from pyrogram import Client
+from pytgcalls import idle
 from pytgcalls import PyTgCalls
+from Zaid.Database.clientdb import get_assistant, save_assistant
 
 bot = Client(
     ":memory:",
@@ -16,17 +18,102 @@ bot = Client(
     plugins={"root": "Zaid.Player"},
 )
 
+
 BOT = TelegramClient('BOT', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-user = Client(
-    SESSION_NAME,
-    api_id=API_ID,
-    api_hash=API_HASH,
-)
 
-Test = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'Zaid.Player'})
-call_py = PyTgCalls(
-    Test,
-    cache_duration=100,
-    overload_quiet_mode=True,
-)
+
+if not SESSION_NAME:
+   ASS_CLI_1 = None
+else:   
+   ASS_CLI_1 = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH)
+
+if not SESSION2:
+   user = None
+else:
+   user = Client(SESSION2, api_id=API_ID, api_hash=API_HASH)
+
+if not SESSION3:
+   user3 = None
+else:
+   user3 = Client(SESSION3, api_id=API_ID, api_hash=API_HASH)
+if not SESSION2:
+   user4 = None
+else:
+   user4 = Client(SESSION4, api_id=API_ID, api_hash=API_HASH)
+if not SESSION2:
+   user5 = None
+else:
+   user5 = Client(SESSION5, api_id=API_ID, api_hash=API_HASH)
+
+Test = ASS_CLI_1
+ASS_CLI_2 = user
+call_py = PyTgCalls(Test)
+call_py2 = PyTgCalls(user)
+call_py3 = PyTgCalls(user3)
+call_py4 = PyTgCalls(user4)
+call_py5 = PyTgCalls(user5)
+
+ASSIDS = []
+ASSID1 = 0
+ASSNAME1 = ""
+ASSUSERNAME1 = ""
+ASSMENTION1 = ""
+ASSID2 = 0
+ASSNAME2 = ""
+ASSUSERNAME2 = ""
+ASSMENTION2 = ""
+ASSID3 = 0
+ASSNAME3 = ""
+ASSUSERNAME3 = ""
+ASSMENTION3 = ""
+ASSID4 = 0
+ASSNAME4 = ""
+ASSUSERNAME4 = ""
+ASSMENTION4 = ""
+ASSID5 = 0
+ASSNAME5 = ""
+ASSUSERNAME5 = ""
+ASSMENTION5 = ""
+random_assistant = []
+
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    global ASSIDS
+    global ASSID1, ASSNAME1, ASSMENTION1, ASSUSERNAME1
+    global ASSID2, ASSNAME2, ASSMENTION2, ASSUSERNAME2
+    global ASSID3, ASSNAME3, ASSMENTION3, ASSUSERNAME3
+    global ASSID4, ASSNAME4, ASSMENTION4, ASSUSERNAME4
+    global ASSID5, ASSNAME5, ASSMENTION5, ASSUSERNAME5
+    await bot.start()
+    if SESSION_NAME != "None":
+       await Test.start()
+       await call_py.start()
+       random_assistant.append(1)
+    if SESSION2 != "None":
+       await user.start()
+       await call_py2.start()
+       random_assistant.append(2)
+    if SESSION3 != "None":
+       await user3.start()
+       await call_py3.start()
+       random_assistant.append(3)
+    if SESSION4 != "None":
+       await user4.start()
+       await call_py4.start()
+       random_assistant.append(4)
+    if SESSION5 != "None":
+       await user5.start()
+       await call_py5.start()
+       random_assistant.append(5)
+    botinfo = bot.get_me()
+    print("[INFO]: STARTING PYTGCALLSS CLIENT")
+    await idle()
+
+
+
+def init_db():
+    global db_mem
+    db_mem = {}
+
+init_db()
--- Zaid/utils.py
+++ Zaid/utils.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-from Zaid.main import bot, call_py
+from Zaid.main import bot, call_py, call_py2, call_py3, call_py4, call_py5
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from Zaid.queues import QUEUE, clear_queue, get_queue, pop_an_item
@@ -10,6 +10,8 @@ from pytgcalls.types.input_stream.quality import (
     LowQualityVideo,
     MediumQualityVideo,
 )
+
+from Zaid.Database.active import remove_active_video_chat, remove_active_chat
 from pyrogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
@@ -18,6 +20,8 @@ from pyrogram.types import (
 )
 from pyrogram import Client, filters
 from pytgcalls.types.stream import StreamAudioEnded, StreamVideoEnded
+
+from Zaid.Database.clientdb import *
 
 
 keyboard = InlineKeyboardMarkup(
@@ -31,10 +35,23 @@ keyboard = InlineKeyboardMarkup(
 
 
 async def skip_current_song(chat_id):
+    _assistant = await get_assistant(chat_id, "assistant")
+    assistant = _assistant["saveassistant"]
     if chat_id in QUEUE:
         chat_queue = get_queue(chat_id)
         if len(chat_queue) == 1:
-            await call_py.leave_group_call(chat_id)
+            if int(assistant) == 1:
+               await call_py.leave_group_call(chat_id)
+            if int(assistant) == 2:
+               await call_py2.leave_group_call(chat_id)
+            if int(assistant) == 3:
+               await call_py3.leave_group_call(chat_id)
+            if int(assistant) == 4:
+               await call_py4.leave_group_call(chat_id)
+            if int(assistant) == 5:
+               await call_py5.leave_group_call(chat_id)
+            await remove_active_video_chat(chat_id)
+            await remove_active_chat(chat_id)
             clear_queue(chat_id)
             return 1
         else:
@@ -45,12 +62,41 @@ async def skip_current_song(chat_id):
                 type = chat_queue[1][3]
                 Q = chat_queue[1][4]
                 if type == "Audio":
-                    await call_py.change_stream(
-                        chat_id,
-                        AudioPiped(
-                            url,
-                        ),
-                    )
+                    if int(assistant) == 1:
+                       await call_py.change_stream(
+                           chat_id,
+                           AudioPiped(
+                               url,
+                           ),
+                       )
+                    if int(assistant) == 2:
+                       await call_py2.change_stream(
+                           chat_id,
+                           AudioPiped(
+                               url,
+                           ),
+                       )
+                    if int(assistant) == 3:
+                       await call_py3.change_stream(
+                           chat_id,
+                           AudioPiped(
+                               url,
+                           ),
+                       )
+                    if int(assistant) == 4:
+                       await call_py4.change_stream(
+                           chat_id,
+                           AudioPiped(
+                               url,
+                           ),
+                       )
+                    if int(assistant) == 5:
+                       await call_py5.change_stream(
+                           chat_id,
+                           AudioPiped(
+                               url,
+                           ),
+                       )
                 elif type == "Video":
                     if Q == 720:
                         hm = HighQualityVideo()
@@ -58,13 +104,39 @@ async def skip_current_song(chat_id):
                         hm = MediumQualityVideo()
                     elif Q == 360:
                         hm = LowQualityVideo()
-                    await call_py.change_stream(
-                        chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
-                    )
+                    if int(assistant) == 1:
+                       await call_py.change_stream(
+                           chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
+                       )
+                    if int(assistant) == 2:
+                       await call_py2.change_stream(
+                           chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
+                       )
+                    if int(assistant) == 3:
+                       await call_py3.change_stream(
+                           chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
+                       )
+                    if int(assistant) == 4:
+                       await call_py4.change_stream(
+                           chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
+                       )
+                    if int(assistant) == 5:
+                       await call_py5.change_stream(
+                           chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
+                       )
                 pop_an_item(chat_id)
                 return [songname, link, type]
             except:
-                await call_py.leave_group_call(chat_id)
+                if int(assistant) == 1:
+                   await call_py.leave_group_call(chat_id)
+                if int(assistant) == 2:
+                   await call_py2.leave_group_call(chat_id)
+                if int(assistant) == 3:
+                   await call_py3.leave_group_call(chat_id)
+                if int(assistant) == 4:
+                   await call_py4.leave_group_call(chat_id)
+                if int(assistant) == 5:
+                   await call_py5.leave_group_call(chat_id)
                 clear_queue(chat_id)
                 return 2
     else:
@@ -91,8 +163,52 @@ async def kicked_handler(_, chat_id: int):
     if chat_id in QUEUE:
         clear_queue(chat_id)
 
+@call_py2.on_kicked()
+async def kicked_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py3.on_kicked()
+async def kicked_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py4.on_kicked()
+async def kicked_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py5.on_kicked()
+async def kicked_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+
+
 
 @call_py.on_closed_voice_chat()
+async def closed_voice_chat_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+
+@call_py2.on_closed_voice_chat()
+async def closed_voice_chat_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py3.on_closed_voice_chat()
+async def closed_voice_chat_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+
+@call_py4.on_closed_voice_chat()
+async def closed_voice_chat_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py5.on_closed_voice_chat()
 async def closed_voice_chat_handler(_, chat_id: int):
     if chat_id in QUEUE:
         clear_queue(chat_id)
@@ -103,21 +219,55 @@ async def left_handler(_, chat_id: int):
     if chat_id in QUEUE:
         clear_queue(chat_id)
 
+@call_py2.on_left()
+async def left_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py3.on_left()
+async def left_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py4.on_left()
+async def left_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+@call_py5.on_left()
+async def left_handler(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
 
 @call_py.on_stream_end()
 async def stream_end_handler(_, u: Update):
-    if isinstance(u, StreamAudioEnded):
-        chat_id = u.chat_id
-        print(chat_id)
-        op = await skip_current_song(chat_id)
-        if op==1:
-           await bot.send_message(chat_id, "✅ **userbot has disconnected from video chat.**")
-        elif op==2:
-           await bot.send_message(chat_id, "❌ **an error occurred**\n\n» **Clearing** __Queues__ **and leaving video chat.**")
-        else:
-         await bot.send_message(chat_id, f"💡 **Streaming next track**\n\n🏷 **Name:** [{op[0]}]({op[1]}) | `{op[2]}`\n💭 **Chat:** `{chat_id}`", disable_web_page_preview=True, reply_markup=keyboard)
-    else:
-       pass
+    chat_id = u.chat_id
+    print(chat_id)
+    await skip_current_song(chat_id)
+
+@call_py2.on_stream_end()
+async def stream_end_handler(_, u: Update):
+    chat_id = u.chat_id
+    print(chat_id)
+    await skip_current_song(chat_id)
+
+@call_py3.on_stream_end()
+async def stream_end_handler(_, u: Update):
+    chat_id = u.chat_id
+    print(chat_id)
+    await skip_current_song(chat_id)
+
+@call_py4.on_stream_end()
+async def stream_end_handler(_, u: Update):
+    chat_id = u.chat_id
+    print(chat_id)
+    await skip_current_song(chat_id)
+
+@call_py5.on_stream_end()
+async def stream_end_handler(_, u: Update):
+    chat_id = u.chat_id
+    print(chat_id)
+    await skip_current_song(chat_id)
 
 
 async def bash(cmd):
