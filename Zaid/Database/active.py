311b3abf957a51360456be3411c051781466a3eb
Zaid <itzzaid@pm.me>
2022-06-19 12:38:25 +0530

    Update active.py

--- Zaid/Database/active.py
+++ Zaid/Database/active.py
@@ -2,6 +2,7 @@
 
 active = []
 activevideo = []
+zaid = []
 
 async def get_active_chats() -> list:
     return active
@@ -18,10 +19,23 @@ async def add_active_chat(chat_id: int):
     if chat_id not in active:
         active.append(chat_id)
 
-
 async def remove_active_chat(chat_id: int):
     if chat_id in active:
         active.remove(chat_id)
+
+# Raids
+async def get_raid_chats() -> list:
+    return zaid
+
+async def add_raid_chat(chat_id: int):
+    if chat_id not in zaid:
+        zaid.append(chat_id)
+
+async def remove_raid_chat(chat_id: int):
+    if chat_id in zaid:
+        zaid.remove(chat_id)
+
+
 
 
 # Active Video Chats
