b3da6f441649d50c692e22f29eca511278fc2adf
Zaid <itzzaid@pm.me>
2022-06-23 01:33:54 +0530

    Update __init__.py

--- Zaid/Database/__init__.py
+++ Zaid/Database/__init__.py
@@ -6,3 +6,11 @@ from config import MONGO_DB_URL
 client = MongoClient(MONGO_DB_URL)
 dbd = client["zaid"]
 db = dbd
+
+
+from motor.motor_asyncio import AsyncIOMotorClient as Bot
+from config import MONGO_DB_URL as tmo
+
+
+MONGODB_CLI = Bot(tmo)
+dbb = MONGODB_CLI.program
