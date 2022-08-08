628cbf1c19d9e2ae27dfdea7e95ad1f43028b8f5
Zaid <itsunknown122@gmail.com>
2022-08-08 22:57:59 +0530

    Add files via upload

--- Heroku/core/__init__.py
+++ Heroku/core/__init__.py
@@ -1,1 +1,6 @@
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from Heroku.config import MONGO_DB_URI
 
+
+MONGODB_CLI = MongoClient(MONGO_DB_URI)
+db = MONGODB_CLI.wbb
---
+++ Heroku/core/chats.py
@@ -0,0 +1,57 @@
+from typing import Dict, List, Union
+
+from Heroku.core import db
+
+
+chatsdb = db.chats
+blacklist_chatdb = db.blacklistChat
+
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def get_served_chats() -> list:
+    chats = chatsdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+
+
+async def remove_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if not is_served:
+        return
+    return await chatsdb.delete_one({"chat_id": chat_id})
+
+
+async def blacklisted_chats() -> list:
+    chats = blacklist_chatdb.find({"chat_id": {"$lt": 0}})
+    return [chat["chat_id"] for chat in await chats.to_list(length=1000000000)]
+
+
+async def blacklist_chat(chat_id: int) -> bool:
+    if not await blacklist_chatdb.find_one({"chat_id": chat_id}):
+        await blacklist_chatdb.insert_one({"chat_id": chat_id})
+        return True
+    return False
+
+
+async def whitelist_chat(chat_id: int) -> bool:
+    if await blacklist_chatdb.find_one({"chat_id": chat_id}):
+        await blacklist_chatdb.delete_one({"chat_id": chat_id})
+        return True
+    return False
