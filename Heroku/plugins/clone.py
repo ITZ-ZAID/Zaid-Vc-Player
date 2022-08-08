3e2d66848781950541bd8bcdba1fa1f6f77fc1b4
Zaid <itsunknown122@gmail.com>
2022-08-08 22:57:16 +0530

    Add files via upload

---
+++ Heroku/plugins/clone.py
@@ -0,0 +1,46 @@
+import os
+import re
+import asyncio
+import time
+from pyrogram import *
+from pyrogram.types import *
+from random import choice
+from Heroku import cloner, ASSUSERNAME, BOT_NAME
+from Heroku.config import API_ID, API_HASH
+IMG = ["https://telegra.ph/file/cefd3211a5acdcd332415.jpg", "https://telegra.ph/file/30d743cea510c563af6e3.jpg", "https://telegra.ph/file/f7ae22a1491f530c05279.jpg", "https://telegra.ph/file/2f1c9c98452ae9a958f7d.jpg"]
+MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
+
+@cloner.on_message(filters.private & filters.command("start"))
+async def hello(client, message: Message):
+    buttons = [
+           [
+                InlineKeyboardButton("✘ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url="t.me/TheUpdatesChannel"),
+            ],
+            [
+                InlineKeyboardButton("✘ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="t.me/TheSupportChat"),
+            ],
+            ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_photo(message.chat.id, f"{choice(IMG)}", caption=MESSAGE, reply_markup=reply_markup)
+
+##Copy from here 
+
+# © By Itz-Zaid Your motherfucker if uh Don't gives credits.
+@cloner.on_message(filters.private & filters.command("clone"))
+async def clone(bot, msg: Message):
+    chat = msg.chat
+    text = await msg.reply("Usage:\n\n /clone token")
+    cmd = msg.command
+    phone = msg.command[1]
+    try:
+        await text.edit("Booting Your Client")
+                   # change this Directry according to ur repo
+        client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "Heroku.modules"})
+        await client.start()
+        user = await client.get_me()
+        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @{ASSUSERNAME} To Your Chat!\n\nThanks for Cloning.")
+    except Exception as e:
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+#End
+##This code fit with every pyrogram Codes just import then @Client Xyz!
+
