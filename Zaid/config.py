e230364fb5aaa6f6bcb7077a9633ba261e3e2b39
Zaid <itzzaid@pm.me>
2022-08-21 08:36:52 +0530

    Update config.py

--- Zaid/config.py
+++ Zaid/config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', "3122923"))
+API_ID = int(getenv("API_ID", "6435225"))
 API_HASH = getenv('API_HASH')
 DURATION_LIMIT = int(getenv('DURATION_LIMIT', '540000'))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ !').split())
