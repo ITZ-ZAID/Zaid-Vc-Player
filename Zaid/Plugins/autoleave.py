cac55d794eb0d8a3d88f5d121bd38a4d914daf50
Zaid <itzzaid@pm.me>
2022-09-04 13:35:19 +0530

    Create autoleave.py

---
+++ Zaid/Plugins/autoleave.py
@@ -0,0 +1,35 @@
+import asyncio
+from pyrogram.errors import PeerIdInvalid, UserNotParticipant
+from Zaid.MusicUtilities.tgcallsrun.music import pytgcalls, smexy as user
+from Zaid.config import LOG_GROUP_ID, AUTO_LEAVE, AUTO_LEAVE_TIME
+
+async def leave_from_inactive_call():
+  if AUTO_LEAVE == True:
+   while not await asyncio.sleep(AUTO_LEAVE_TIME):
+    all_chat_id = []
+    async for chat in user.iter_dialogs():
+        chat_id = chat.chat.id
+        if chat_id != LOG_GROUP_ID:
+          if chat.chat.type in ["group", "supergroup"]:
+            for call in pytgcalls.calls:
+                call_chat_id = int(getattr(call, "chat_id"))
+                if call_chat_id in all_chat_id:
+                    pass
+                else:
+                    all_chat_id.append(call_chat_id)
+                call_status = getattr(call, "status")
+                try:
+                    if call_chat_id == chat_id and call_status == "not_playing":
+                        await user.leave_chat(chat_id)
+                    elif chat_id not in all_chat_id:
+                        await user.leave_chat(chat_id)
+                except UserNotParticipant:
+                    pass
+            if chat_id not in all_chat_id:
+                try:
+                    await user.leave_chat(chat_id)
+                except (PeerIdInvalid, UserNotParticipant):
+                    pass
+
+
+asyncio.create_task(leave_from_inactive_call())
