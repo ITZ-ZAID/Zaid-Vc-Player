7ef8a9c4a596e3949d0fc55b3b650764e9b11cd7
Stark Industries TG <88097120+StarkIndustriesTG@users.noreply.github.com>
2022-09-20 10:12:10 +0530

    Fixed PeerID Invalid

--- Heroku/plugins/clone.py
+++ Heroku/plugins/clone.py
@@ -6,6 +6,7 @@ from pyrogram import *
 from pyrogram.types import *
 from random import choice
 from Heroku import cloner, ASSUSERNAME, BOT_NAME
+from Heroku import client as sys
 from Heroku.config import API_ID, API_HASH
 IMG = ["https://telegra.ph/file/cefd3211a5acdcd332415.jpg", "https://telegra.ph/file/30d743cea510c563af6e3.jpg", "https://telegra.ph/file/f7ae22a1491f530c05279.jpg", "https://telegra.ph/file/2f1c9c98452ae9a958f7d.jpg"]
 MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
@@ -39,6 +40,8 @@ async def clone(bot, msg: Message):
         await client.start()
         user = await client.get_me()
         await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @{ASSUSERNAME} To Your Chat!\n\nThanks for Cloning.")
+        APP_USERNAME = user.username
+        await sys.send_message(APP_USERNAME, "/start")
     except Exception as e:
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
 #End
